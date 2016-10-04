6bc353aa8438a5732825494098455829a68b9177
yuk4142 <yuk4142@HRH-IC0027319.wrdsb.ca>
2016-10-04 14:19:09 -0400

    done

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -6,7 +6,7 @@
 
 /**
  *
- * @author PUT_YOUR_NAME_HERE
+ * @author kevin yu
  */
 public class Quiz2 {
 
@@ -19,7 +19,13 @@ public class Quiz2 {
      * @return 
      */
     public int sumUpTo(int n){
-        
+        //base case for if n equals 0, whch means there is nothing left to add onto sum
+        if(n == 0){
+            //return n(or 0)
+            return n;
+        }
+        //return the first value of n, followed by adding the recursive function of n-1
+        return n + sumUpTo(n-1);
     }
     
     /**
@@ -30,6 +36,23 @@ public class Quiz2 {
      */
     public String reverseString(String word){
         
+        //create middle integer that divides the length of the s string by 2, to find how many spots the word has left
+        int middle = word.length() / 2;
+        
+        //base case for when the method reaches the middle of the word/is done reversing word
+        if (middle == 0) {
+            //return true boolean, because the word is a palindrome
+            return word;
+        }
+       //create string for holding the left most letter of the s string
+        String firstLetter = word.substring(0, 1);
+        //create string for holding the right most letter of the s string
+        String lastLetter = word.substring(word.length() - 1, word.length());
+        
+        //return the last letter in the first position, and the first letter in the last position to perform swap
+        //and add the recursive swapping of the next set of letters in, placed in the middle of the
+       //last and first letter
+        return lastLetter + reverseString(word.substring(1, word.length() - 1)) + firstLetter;
     }
     
     
@@ -40,6 +63,8 @@ public class Quiz2 {
         // Use this section for conducting tests
         Quiz2 test = new Quiz2();
         
+        System.out.println(test.sumUpTo(5));
+        System.out.println(test.reverseString("nam noitaripsni eht sined"));
         //use test.sumUpTo(__)  or test.reverseString(___) to test
         
     }
